966946a8e97308996f6e9f757cd89a566668cc6d
552020 <stefanolombardo@posteo.de>
2023-06-24 21:02:56 +0200

    feature: display single item details on its own page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
 
-async function fetchData(searchTerm: string, isRealApi: boolean, page: number) {
+export type Item = {
+  identifier: number;
+  title: string;
+  link: string;
+  subject: string;
+  description: string;
+  date: string;
+  premium: number;
+  image_url: string;
+  teaser_image_urls: Array<{ width: number; src: string }>;
+};
+
+export type QueryData = {
+  items: Item[];
+};
+
+export async function fetchData(searchTerm: string, isRealApi: boolean, page: number) {
   try {
     let res;
     if (isRealApi) {
@@ -56,62 +73,72 @@ export default function Home() {
 
   return (
     <div className="h-screen bg-white">
-      <button
-        type="button"
-        onClick={() => setIsRealApi(!isRealApi)}
-        className="absolute right-4 top-4 p-2 text-xs text-white bg-blue-500 rounded hover:bg-blue-600"
-      >
-        {isRealApi ? "Switch to Local API" : "Switch to Real API"}
-      </button>
-
-      <form onSubmit={onSubmit} className="flex flex-col items-center justify-center h-full p-6 space-y-4">
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="w-full p-2 border rounded"
-        />
-
-        <button type="submit" className="w-full p-2 text-white bg-green-500 rounded hover:bg-green-600">
-          Submit
+      <nav className="bg-slate-300 h-16">Navigation</nav>
+      <div className="container mx-auto py-4">
+        <button
+          type="button"
+          onClick={() => setIsRealApi(!isRealApi)}
+          className="absolute right-4 top-4 p-2 text-xs text-white bg-blue-500 rounded hover:bg-blue-600"
+        >
+          {isRealApi ? "Switch to Local API" : "Switch to Real API"}
         </button>
 
-        {isLoading && <div>Loading...</div>}
-        {isError && (
-          <div>
-            <span>isError-</span> <span>Error: {(error as Error).message}</span>
-          </div>
-        )}
-        {data &&
-          data.map((result: any, index: number) => (
-            <div key={index} className="p-2 border rounded">
-              {result.title}
+        <form onSubmit={onSubmit} className="flex flex-col items-center justify-center h-full p-6 space-y-4 mt-4">
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="w-full p-2 border rounded"
+          />
+
+          <button type="submit" className="w-full p-2 text-white bg-green-500 rounded hover:bg-green-600">
+            Submit
+          </button>
+
+          {isLoading && <div>Loading...</div>}
+          {isError && (
+            <div>
+              <span>isError-</span> <span>Error: {(error as Error).message}</span>
             </div>
-          ))}
-        <div className="mt-4">
-          <button
-            type="button"
-            disabled={page === 0}
-            onClick={() => setPage(page - 1)}
-            className="mr-2 p-2 text-white bg-blue-500 rounded hover:bg-blue-600"
-          >
-            Previous Page
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              if (!isPreviousData) {
-                setPage(page + 1);
-              }
-            }}
-            disabled={isPreviousData || isFetching}
-            className="p-2 text-white bg-blue-500 rounded hover:bg-blue-600"
-          >
-            Next Page
-          </button>
-          {isFetching ? <span>Loading...</span> : null}
-        </div>
-      </form>
+          )}
+
+          {data &&
+            data.map((item: Item) => (
+              <div key={item.identifier} className="border-2 border-gray-200 my-4 p-4 rounded">
+                <Link
+                  href={`/statistic/${item.identifier}?searchTerm=${searchTerm}&isRealApi=${isRealApi}&page=${page}`}
+                  className="text-blue-500 hover:underline"
+                >
+                  {item.title}
+                </Link>
+                <p>{item.description}</p>
+              </div>
+            ))}
+          <div className="mt-4">
+            <button
+              type="button"
+              disabled={page === 0}
+              onClick={() => setPage(page - 1)}
+              className="mr-2 p-2 text-white bg-blue-500 rounded hover:bg-blue-600"
+            >
+              Previous Page
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                if (!isPreviousData) {
+                  setPage(page + 1);
+                }
+              }}
+              disabled={isPreviousData || isFetching}
+              className="p-2 text-white bg-blue-500 rounded hover:bg-blue-600"
+            >
+              Next Page
+            </button>
+            {isFetching ? <span>Loading...</span> : null}
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/pages/statistic/[id].tsx
@@ -0,0 +1,53 @@
+import { useRouter } from "next/router";
+import { useQueryClient } from "@tanstack/react-query";
+import Link from "next/link";
+import { QueryData, Item } from "../index";
+
+const StatisticItem: React.FC<{ item: Item }> = ({ item }) => {
+  const imageOnError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    e.currentTarget.src = item.teaser_image_urls.reduce((prev, curr) => (prev.width > curr.width ? prev : curr)).src;
+  };
+
+  return (
+    <div className="border-2 border-gray-200 my-4 p-4 rounded">
+      <Link href={item.link} className="text-2xl mb-4 text-blue-500 hover:underline">
+        {item.title}
+      </Link>
+      {item.premium === 1 && <span className="inline-block bg-red-500 text-white px-2 py-1 ml-2 rounded">Premium</span>}
+      <p>{item.subject}</p>
+      <p>{item.description}</p>
+      <p>{new Date(item.date).toLocaleDateString()}</p>
+      <img src={item.image_url} alt={item.title} onError={imageOnError} />
+    </div>
+  );
+};
+
+export default function Statistic() {
+  const router = useRouter();
+  const { id, searchTerm, isRealApi, page } = router.query;
+  const queryClient = useQueryClient();
+
+  const allData = queryClient.getQueryData<QueryData>(["statistics", searchTerm, isRealApi === "true", Number(page)]);
+
+  //   console.log("allData", allData);
+
+  let item;
+  if (Array.isArray(allData)) {
+    item = (allData as Item[]).find((item: Item) => item.identifier === Number(id));
+  }
+
+  //   console.log("item", item);
+
+  if (!item) {
+    return <p>No data found</p>;
+  }
+
+  return (
+    <div className="container mx-auto px-4">
+      <StatisticItem item={item} />
+      <Link href="/" className="text-blue-500 hover:underline">
+        Back to list
+      </Link>
+    </div>
+  );
+}
